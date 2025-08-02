6ef6cbe2bc21a8d70513c95454a2c8699537692d
Ed__xu__Ed <m.tofu@qq.com>
2025-08-02 21:36:36 +0800

    add dockercompose

--- latitude7390/install.sh
+++ latitude7390/install.sh
@@ -38,3 +38,4 @@ systemctl start docker.service
 chmod a+rw /var/run/docker.sock
 docker run -itd --name redis -p 6379:6379 redis
 docker run -itd --name mysql8 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 mysql:8.0
+yes | yay -S docker-compose
